0be4aa9fd832c248c309a7754c42ab763f637d41
ArtemDev <Artem35953@gmail.com>
2023-10-23 11:30:44 +0300

    added contact page

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -5,8 +5,34 @@ use yii\helpers\Html;
 $this->title = 'Контакты';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="container">
-<h1>
-    <?= Html::encode($this->title); ?>
-</h1>
-</div>
+
+<section class="contacts">
+  <div class="container">
+    <h1>
+      <?= Html::encode($this->title); ?>
+    </h1>
+    <div class="contacts__wrap">
+      <div class="contacts__info">
+        <div class="contacts__item">
+          <div class="contacts__item-title">Адрес ЖК</div>
+          <div class="contacts__item-text">
+            <?= Yii::$app->configManager->getItemValue('contactAddress'); ?>
+          </div>
+        </div>
+        <div class="contacts__item">
+          <div class="contacts__item-title">Телефон</div>
+          <a class="contacts__item-text" href="tel:+78792261420">+7 (879) 226-14-20</a>
+        </div>
+        <div class="contacts__item">
+          <div class="contacts__item-title">E-mail</div>
+          <a class="contacts__item-text" href="mailto:Dom-Kliuch@yandex.ru">Dom-Kliuch@yandex.ru</a>
+        </div>
+      </div>
+      <div id="map" style="background-color: grey;">
+        
+      </div>
+    </div>
+  </div>
+</section>
+ 
+  
