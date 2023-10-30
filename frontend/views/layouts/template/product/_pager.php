8f4ba14ff03b3f12dc52b9d6b1a0b3e83afcb740
ArtemDev <Artem35953@gmail.com>
2023-10-30 15:08:48 +0300

    exedent pagination widget

--- frontend/views/layouts/template/product/_pager.php
+++ frontend/views/layouts/template/product/_pager.php
@@ -1,24 +1,23 @@
 <?php
 
-use yii\widgets\LinkPager;
+use common\models\NoWrapLinkPager;
 ?>
 
 
-<?= LinkPager::widget([
+
+<?= NoWrapLinkPager::widget([
   'pagination' => $dataProvider->pagination,
   'disableCurrentPageButton' => false,
-  'activePageCssClass' => 'pagination__item pagination__item--active',
-  'disabledPageCssClass' => 'pagination__item pagination__item--disable',
+  'activePageCssClass' => 'pagination__item--active',
+  'disabledPageCssClass' => 'pagination__item--disable',
   'pageCssClass' => 'pagination__item',
   'prevPageCssClass' => 'pagination__item pagination__item--arrow',
   'nextPageCssClass' => 'pagination__item pagination__item--arrow',
   'nextPageLabel' => '<img src="/static/sprite.svg#arrow_n" />',
   'prevPageLabel' => '<img src="/static/sprite.svg#arrow_p" />',
   'options' => [
-      'tag' => 'div',
-      'class' => 'pagination',
+    'tag' => 'div',
+    'class' => 'pagination',
   ],
-  'linkContainerOptions' => [
-    'tag' => 'span',
-  ],
+
 ]); ?>
