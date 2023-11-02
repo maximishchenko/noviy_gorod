f5d4a57ff6c07809bed3791b4efb3026a65d1c69
Maxim Ishchenko <maxim.ishchenko@gmail.com>
2023-11-02 17:25:24 +0300

    Assets Bundle

--- frontend/modules/content/controllers/ParkingController.php
+++ frontend/modules/content/controllers/ParkingController.php
@@ -4,12 +4,13 @@ namespace frontend\modules\content\controllers;
 
 use frontend\controllers\BaseController;
 use frontend\modules\content\models\Parking;
+use Yii;
 
 class ParkingController extends BaseController
 {
     public function actionIndex()
     {
-        $activeItemId = 1;
+        $activeItemId = Yii::$app->configManager->getItemValue('contentParkingStage');
 
         $activeItem = Parking::find()->active()->where(['id' => $activeItemId])->one();
         return $this->render('index', ['activeItem' => $activeItem]);
