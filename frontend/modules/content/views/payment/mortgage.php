ef008b4acc2e309fedb50ec7e23dba05042ffe32
ArtemDev <Artem35953@gmail.com>
2023-11-15 15:32:51 +0300

    added mortgage page

--- frontend/modules/content/views/payment/mortgage.php
+++ frontend/modules/content/views/payment/mortgage.php
@@ -4,8 +4,121 @@ $this->title = 'Ипотека';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<div class="container">
+<section class="mortgage">
+  <div class="container">
     <h1>
       <?= $this->title; ?>
     </h1>
-</div>
+
+    <div class="mortgage__wrap">
+      <div class="mortgage__item">
+        <div class="mortgage__item-wrap">
+          <div class="mortgage__item-title">Ипотека с господдержкой 7,7%</div>
+          <div class="mortgage__item-text">
+            <p>
+              Совместно со Сбером мы предоставляем возможность приобрести квартиру на льготных условиях со ставкой от 7,7% годовых. Главное преимущество программы — она доступна для всех категорий заемщиков.
+            </p>
+
+            <p>
+              Купить квартиру в ипотеку с господдержкой можно при максимальной сумме кредита в 6 000 000 рублей. Первоначальный взнос <meta charset="utf-8" /><b id="docs-internal-guid-b8dcc144-7fff-eb42-fb23-57301c8a2244">—</b>&nbsp;от 15%.
+              Срок кредита <meta charset="utf-8" /><b id="docs-internal-guid-b8dcc144-7fff-eb42-fb23-57301c8a2244">—</b>&nbsp;до 30 лет.
+            </p>
+
+            <p>
+              Воспользоваться программой могут все граждане РФ старше 18 лет. Условие - ипотечный кредит по данной программе должен быть один. Купить сразу две квартиры в ипотеку с господдержкой не получится.
+            </p>
+
+            <p>Какие документы необходимы для покупки квартиры в ипотеку с господдержкой?</p>
+
+            <ul>
+              <li>Паспорт заемщика/созаемщика с отметкой о регистрации</li>
+              <li>При наличии временной регистрации - документ, подтверждающий регистрацию по месту пребывания</li>
+              <li>Документы, подтверждающие финансовое состояние и трудовую занятость заемщика/созаемщика</li>
+              <li>СНИЛС - страховой номер индивидуального лицевого счёта</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="mortgage__item">
+        <div class="mortgage__item-wrap">
+          <div class="mortgage__item-title">Семейная ипотека 5,7%</div>
+          <div class="mortgage__item-text">
+            <p>Для семей, где хотя бы один ребёнок родился после 2018 года, доступна ипотека со сниженной ставкой 5,7% от Сбера. Также воспользоваться программой могут семьи с ребенком-инвалидом.</p>
+
+            <ul>
+              <li>Первоначальный взнос <meta charset="utf-8" /><b id="docs-internal-guid-b8dcc144-7fff-eb42-fb23-57301c8a2244">—&nbsp;</b>от 15%</li>
+              <li>Срок кредита <meta charset="utf-8" /><b id="docs-internal-guid-b8dcc144-7fff-eb42-fb23-57301c8a2244">—</b> до 30 лет</li>
+              <li>Сумма кредита <meta charset="utf-8" /><b id="docs-internal-guid-b8dcc144-7fff-eb42-fb23-57301c8a2244">—</b> до 6 млн рублей</li>
+            </ul>
+
+            <p>Получить ипотеку с льготной ставкой могут семьи с двумя и более детьми младше 18 лет, а также семьи, у которых в период с 1 января 2018 года до 31 декабря 2023 года родился хотя бы один ребенок.</p>
+
+            <p>Программой также могут воспользоваться семьи с ребенком-инвалидом. Необходимо будет предоставить справку от государственного учреждения медико-социальной экспертизы.</p>
+          </div>
+        </div>
+      </div>
+      <div class="mortgage__item">
+        <div class="mortgage__item-wrap">
+          <div class="mortgage__item-title">IT-ипотека 4,7%</div>
+          <div class="mortgage__item-text">
+            <p>Специалисты сферы информационных технологий могут воспользоваться льготными условиями приобретения квартиры от «ЮгСтройИнвест». При оформлении в Сбере ставка составит 4,7%.</p>
+
+            <p>Чтобы заемщик мог воспользоваться предложением, компания должна находиться в России и иметь аккредитацию Минцифры РФ. С учетом НДФЛ средняя зарплата за последние 3 месяца и более должна быть от 120 000 рублей в месяц.</p>
+
+            <ul>
+              <li>Первоначальный взнос - от 15%</li>
+              <li>Срок кредита - до 30 лет</li>
+              <li>Сумма кредита - до 9 млн рублей</li>
+            </ul>
+
+            <p>Для подтверждения трудоустройства в компании, при оформлении необходимо предоставить копию трудовой книжки.</p>
+          </div>
+        </div>
+      </div>
+      <div class="mortgage__item">
+        <div class="mortgage__item-wrap">
+          <div class="mortgage__item-title">Ипотека на парковочное место</div>
+          <div class="mortgage__item-text">
+            <p>Наша компания работает с ведущими банками и финансовыми учреждениями, чтобы предоставить нашим клиентам наиболее выгодные условия ипотеки.</p>
+
+            <p>Сбербанк предлагает ипотечные кредиты на 30 лет с начальной ставкой от 11,4% и первоначальным взносом от 25%. Это идеальное решение для тех, кто ищет безопасное и выгодное место для хранения своего автомобиля.</p>
+
+            <p>
+              Мы предлагаем нашим клиентам не только гибкие условия погашения кредита, но и высокий уровень сервиса. Наши специалисты помогут вам оформить кредит, подобрать оптимальный вариант ипотеки для ваших потребностей и рассчитать
+              платежи.
+            </p>
+
+            <p>Для получения ипотеки на парковочное место свяжитесь с нашим отделом продаж и уточните все детали. Мы с удовольствием ответим на все ваши вопросы и предоставим вам всю необходимую информацию.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="banks__wraper">
+      <div class="page-title">Аккредитованные банки</div>
+      <div class="bank-list">
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-1_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-2_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-3_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-4_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-5_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-6_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-7_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-8_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-9_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-10_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-11_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-12_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-13_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-14_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-15_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-16_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-17_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-18_0x0_cd6.webp" alt="" /></div>
+        <div class="bank-list__item"><img src="../frontend/web/images/banks/logo-19_0x0_cd6.webp" alt="" /></div>
+      </div>
+    </div>
+  </div>
+    
+
+</section>
