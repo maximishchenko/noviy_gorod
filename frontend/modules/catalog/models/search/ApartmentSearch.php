69db1d71324d2f2e818e9ae82b1e8dd1204157c4
Maxim Ishchenko <maxim.ishchenko@gmail.com>
2024-09-24 13:27:59 +0300

    fix: added extended apartment params into catalog search

--- frontend/modules/catalog/models/search/ApartmentSearch.php
+++ frontend/modules/catalog/models/search/ApartmentSearch.php
@@ -39,7 +39,8 @@ class ApartmentSearch extends Apartment
     public function search(array $params): DataProviderInterface
     {
         $query = Apartment::getDb()->cache(function() {
-            return Apartment::find()->joinWith(['house'])->byLayout()->minFloor()->forSale()->active();
+            // return Apartment::find()->joinWith(['house'])->byLayout()->minFloor()->forSale()->active();
+            return Apartment::find()->joinWith(['house'])->minFloor()->forSale()->active();
         }, Apartment::getCacheDuration(), Apartment::getCacheDependency());
 
         $dataProvider = new ActiveDataProvider([
@@ -57,8 +58,8 @@ class ApartmentSearch extends Apartment
         $dataProvider->setSort([
             'attributes' => [
                 'totalArea' => [
-                    'asc' => [Layout::tableName() . '.total_area' => SORT_ASC],
-                    'desc' => [Layout::tableName() . '.total_area' => SORT_DESC],
+                    'asc' => [Layout::tableName() . '.total_area' => SORT_ASC, Apartment::tableName() . '.extended_count_rooms' => SORT_ASC],
+                    'desc' => [Layout::tableName() . '.total_area' => SORT_DESC, Apartment::tableName() . '.extended_count_rooms' => SORT_DESC],
                     'default' => SORT_ASC
                 ],
             ],
@@ -79,7 +80,8 @@ class ApartmentSearch extends Apartment
             House::tableName() . '.id' => $this->house,
             Layout::tableName() . '.count_rooms' => $this->countRooms,
         ]);
-            
+
+        $query->orFilterWhere([Apartment::tableName() . '.extended_count_rooms' => $this->countRooms]);
         $query->andFilterWhere(['between', Layout::tableName() . '.total_area', $this->minArea, $this->maxArea]);
 
         return $dataProvider;
--- frontend/views/layouts/template/product/_item.php
+++ frontend/views/layouts/template/product/_item.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 ?>
 <a class="flats__item" href="/filter/<?= $model->slug; ?>">
     <div class="flats__item-title">
-        <?= $model->layout->count_rooms; ?>-комнатная квартира <?= $model->layout->total_area; ?> м<sup>2</sup>
+        <?= ($model->extended_count_rooms > 0) ? $model->extended_count_rooms : $model->layout->count_rooms; ?>-комнатная квартира <?= ($model->extended_total_area > 0) ? $model->extended_total_area : $model->layout->total_area; ?> м<sup>2</sup>
     </div>
     <div class="flats__item-image">
         <?= Html::img($model->apartmentPreview, []); ?> <!-- $model->thumb -->
@@ -12,7 +12,7 @@ use yii\helpers\Html;
 
     <div class="flats__item-chars">
         <p>Литер <?= $model->layout->entrance->house->name; ?></p>
-        <p><?= $model->layout->total_area; ?> м<sup>2</sup></p>
+        <p><?= ($model->extended_total_area > 0) ? $model->extended_total_area : $model->layout->total_area; ?> м<sup>2</sup></p>
     </div>
 
     <?php if(isset($showCallbackButton) && !empty(($showCallbackButton))): ?>
